54f3fdd843a23ad2973d9c8afd361f6818615fd6
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-12-22 14:01:06 +0100

    Adding buttons with letters and listening to them.

--- HangMan/app/Game.js
+++ HangMan/app/Game.js
@@ -1,8 +1,22 @@
 class Game {
+  constructor({ outputWrapper, wordWrapper, categoryWrapper, lettersWrapper }) {
+    this.outputWrapper = outputWrapper;
+    this.wordWrapper = wordWrapper;
+    this.categoryWrapper = categoryWrapper;
+    this.lettersWrapper = lettersWrapper;
+  }
+  guess(letter) {
+    console.log(letter);
+  }
   start() {
-    console.log("Hello World");
+    for (let i = 0; i < 26; i++) {
+      const label = (i + 10).toString(36);
+      const button = document.createElement("button");
+      button.innerHTML = label;
+      button.addEventListener("click", () => {
+        this.guess(label);
+      });
+      this.lettersWrapper.appendChild(button);
+    }
   }
 }
-const game = new Game();
-
-game.start();
