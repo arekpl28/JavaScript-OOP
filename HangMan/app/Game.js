1910a08a19d38eb635e61c3dda7f857013979ca3
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-12-22 16:05:52 +0100

    Add winning and loosing

--- HangMan/app/Game.js
+++ HangMan/app/Game.js
@@ -1,6 +1,9 @@
 import { Quote } from "./Quote.js";
 
 class Game {
+  currentStep = 0;
+  lastStep = 7;
+
   quotes = [
     {
       text: "Pan Tatedusz",
@@ -19,6 +22,7 @@ class Game {
       category: "Film",
     },
   ];
+
   constructor({ outputWrapper, wordWrapper, categoryWrapper, lettersWrapper }) {
     this.outputWrapper = outputWrapper;
     this.wordWrapper = wordWrapper;
@@ -30,28 +34,56 @@ class Game {
     this.categoryWrapper.innerHTML = category;
     this.quote = new Quote(text.toLowerCase());
   }
-  guess(letter) {
-    this.quote.guess(letter);
-    this.drawQuote();
+
+  guess(letter, event) {
+    event.target.disabled = true;
+    if (this.quote.guess(letter)) {
+      this.drawQuote();
+    } else {
+      this.currentStep++;
+      document.getElementsByClassName("step")[
+        this.currentStep
+      ].style.opacity = 1;
+      if (this.currentStep == this.lastStep) {
+        this.loosing();
+      }
+    }
   }
+
   drawLetters() {
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
       button.innerHTML = label;
-      button.addEventListener("click", () => {
-        this.guess(label);
+      button.addEventListener("click", (event) => {
+        this.guess(label, event);
       });
       this.lettersWrapper.appendChild(button);
     }
   }
+
   drawQuote() {
     const content = this.quote.getContent();
     this.wordWrapper.innerHTML = content;
+    if (!content.includes("_")) {
+      this.winning();
+    }
   }
+
   start() {
+    document.getElementsByClassName("step")[this.currentStep].style.opacity = 1;
     this.drawLetters();
     this.drawQuote();
+  }
+
+  winning() {
+    this.wordWrapper.innerHTML = "GRATULACJE! WYGRYWASZ! KONIEC GRY";
+    this.lettersWrapper.innerHTML = "";
+  }
+
+  loosing() {
+    this.wordWrapper.innerHTML = "NIESTETY! PRZEGRYWASZ! TO JEST KONIEC GRY";
+    this.lettersWrapper.innerHTML = "";
   }
 }
 
@@ -61,4 +93,5 @@ const game = new Game({
   categoryWrapper: document.getElementById("category"),
   lettersWrapper: document.getElementById("letters"),
 });
+
 game.start();
