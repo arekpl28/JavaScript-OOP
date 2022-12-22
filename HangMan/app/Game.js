8656383e808afc4c1b65988ffd3c71d11d57f129
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-12-22 14:27:26 +0100

    Changes in index.html and adding a class quote

--- HangMan/app/Game.js
+++ HangMan/app/Game.js
@@ -1,14 +1,39 @@
+import { Quote } from "./Quote.js";
+
 class Game {
+  quotes = [
+    {
+      text: "Pan Tatedusz",
+      category: "Utwór literacki",
+    },
+    {
+      text: "Janko muzykant",
+      category: "Utwór literacki",
+    },
+    {
+      text: "Akademia Pana Kleksa",
+      category: "Film",
+    },
+    {
+      text: "U Pana Boga za piecem",
+      category: "Film",
+    },
+  ];
   constructor({ outputWrapper, wordWrapper, categoryWrapper, lettersWrapper }) {
     this.outputWrapper = outputWrapper;
     this.wordWrapper = wordWrapper;
     this.categoryWrapper = categoryWrapper;
     this.lettersWrapper = lettersWrapper;
+
+    const { text, category } =
+      this.quotes[Math.floor(Math.random() * this.quotes.length)];
+    this.categoryWrapper.innerHTML = category;
+    this.quote = new Quote(text);
   }
   guess(letter) {
     console.log(letter);
   }
-  start() {
+  drawLetters() {
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
@@ -19,4 +44,17 @@ class Game {
       this.lettersWrapper.appendChild(button);
     }
   }
+  start() {
+    this.drawLetters();
+    const content = this.quote.getContent();
+    this.wordWrapper.innerHTML = content;
+  }
 }
+
+const game = new Game({
+  outputWrapper: document.getElementById("output"),
+  wordWrapper: document.getElementById("word"),
+  categoryWrapper: document.getElementById("category"),
+  lettersWrapper: document.getElementById("letters"),
+});
+game.start();
