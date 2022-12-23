e3e1765d6aadcd34c430da4119380d933a6be78e
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-12-23 19:31:15 +0100

    Discovering and marking flag of fields on board

--- Minesweeper/js/Game.js
+++ Minesweeper/js/Game.js
@@ -26,6 +26,8 @@ class Game extends UI {
 
   #cells = [];
 
+  #cellsElements = [];
+
   #board = null;
 
   initializeGame() {
@@ -42,12 +44,24 @@ class Game extends UI {
     this.#numberOfCols = cols;
     this.#numberOfMines = mines;
 
+    this.#setStyles();
+
     this.#generateCells();
     this.#renderBoard();
+
+    this.#cellsElements = this.getElements(this.UiSelectors.cell);
+    this.#addCellsEventListeners();
   }
 
   #handleElements() {
     this.#board = this.getElement(this.UiSelectors.board);
+  }
+
+  #addCellsEventListeners() {
+    this.#cellsElements.forEach((element) => {
+      element.addEventListener("click", this.#handleCellClick);
+      element.addEventListener("contextmenu", this.#handleCellContextMenu);
+    });
   }
 
   #generateCells() {
@@ -64,6 +78,34 @@ class Game extends UI {
       this.#board.insertAdjacentHTML("beforeend", cell.createElement());
       cell.element = cell.getElement(cell.selector);
     });
+  }
+
+  #handleCellClick = (e) => {
+    const target = e.target;
+    const rowIndex = parseInt(target.getAttribute("data-y"), 10);
+    const colIndex = parseInt(target.getAttribute("data-x"), 10);
+
+    this.#cells[rowIndex][colIndex].revealCell();
+  };
+
+  #handleCellContextMenu = (e) => {
+    e.preventDefault();
+    const target = e.target;
+    const rowIndex = parseInt(target.getAttribute("data-y"), 10);
+    const colIndex = parseInt(target.getAttribute("data-x"), 10);
+
+    const cell = this.#cells[rowIndex][colIndex];
+
+    if (cell.isReveal) return;
+
+    cell.toggleFlag();
+  };
+
+  #setStyles() {
+    document.documentElement.style.setProperty(
+      "--cells-in-row",
+      this.#numberOfCols
+    );
   }
 }
 
